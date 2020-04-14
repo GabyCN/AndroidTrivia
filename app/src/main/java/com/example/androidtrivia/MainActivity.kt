467d2e8bb3a9448c9ea37510b0000f6353e5477c
GabyCN <Gabynoestaquieta@gmail.com>
2020-04-14 17:31:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.androidtrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.navigation.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
